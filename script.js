cfce1267f301ccd3909561349e55c143960499c8
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-03-07 21:33:26 -0600

    Corrigiendo errores con Math.max y Math.min

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
 function jugar() {
     let numero1 = prompt("Ingrese un número");
-    if (Number(numero1)) {
-        console.log("Número 1: " + numero1);
-    } else {
+    numero1 = Number(numero1);
+    while (isNaN(numero1)) {
         numero1 = prompt("Por favor, ingrese un número real");
-        while (!(Number(numero1))) {
-            numero1 = prompt("Dije: un número real");
-        }
+        numero1 = Number(numero1);
     }
+    console.log("Número 1: " + numero1);
 
     let numero2 = prompt("Ingrese un segundo número");
-    if (Number(numero2)) {
-        console.log("Número 2: " + numero2);
-    } else {
+    numero2 = Number(numero2);
+    while (isNaN(numero2)) {
         numero2 = prompt("Nuevamente, por favor, ingrese un número real");
-        while (!(Number(numero2))) {
-            numero2 = prompt("Cooperemos y coloquemos un número real");
-        }
+        numero2 = Number(numero2);
     }
+    console.log("Número 2: " + numero2);
 
     let numero3 = prompt("Ingrese un tercer número");
-    if (Number(numero3)) {
-        console.log("Número 3: " + numero3);
-    } else {
+    numero3 = Number(numero3);
+    while (isNaN(numero3)) {
         numero3 = prompt("Solo uno más, por favor, hagámoslo bien");
-        while (!(Number(numero3))) {
-            numero3 = prompt("Número real, si es tan amable");
-        }
+        numero3 = Number(numero3);
     }
+    console.log("Número 3: " + numero3);
 
     orden(numero1, numero2, numero3);
     sonIguales(numero1, numero2, numero3);
 }
 
-// Pasados 3 números, devuelve el mayor, el menor y el de en medio
 function orden(numero1, numero2, numero3) {
     let numeroMayor = Math.max(numero1, numero2, numero3);
     let numeroMenor = Math.min(numero1, numero2, numero3);
-    let numeroMedio = (numero1 != numeroMayor && numero1 != numeroMenor) ? numero1 :
-        (numero2 != numeroMayor && numero2 != numeroMenor) ? numero2 :
+    let numeroMedio = (numero1 !== numeroMayor && numero1 !== numeroMenor) ? numero1 :
+        (numero2 !== numeroMayor && numero2 !== numeroMenor) ? numero2 :
         numero3;
 
     console.log(`${numeroMayor}, ${numeroMedio}, ${numeroMenor}`);
@@ -49,7 +42,7 @@ function orden(numero1, numero2, numero3) {
 
 function sonIguales(numero1, numero2, numero3) {
     let mensaje;
-    if (numero1 == numero2 && numero2 == numero3) {
+    if (numero1 === numero2 && numero2 === numero3) {
         mensaje = "¡Todos los números son iguales!";
     } else {
         mensaje = "Los números fueron diferentes";
@@ -100,6 +93,3 @@ function visualizarEnDom(num1, num2, num3) {
     parrafo3.textContent = sonIguales(num1, num2, num3);
     document.body.append(parrafo3);
 }
-
-
-
