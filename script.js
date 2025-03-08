d670d87086c8186acfe5445bca382419b32ae23f
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-03-07 21:41:38 -0600

    Corrección de error

--- script.js
+++ script.js
@@ -46,5 +46,53 @@ function sonIguales(numero1, numero2, numero3) {
     if (numero1 === numero2 && numero2 === numero3) {
         mensaje = "¡Todos los números son iguales!";
     } else {
-        mensaje = "Los números fueron
+        mensaje = "Los números fueron diferentes";
+    }
 
+    console.log(mensaje);
+    return mensaje;
+}
+
+function visualizarEnDom(num1, num2, num3) {
+    const parrafo1 = document.createElement("p");
+    parrafo1.textContent = "Números del mayor al menor: ";
+    document.body.append(parrafo1);
+
+    let ul = document.createElement("ul");
+    document.body.append(ul);
+
+    const mayor = document.createElement("li");
+    mayor.textContent = num1;
+    ul.append(mayor);
+
+    const medio = document.createElement("li");
+    medio.textContent = num2;
+    ul.append(medio);
+
+    const menor = document.createElement("li");
+    menor.textContent = num3;
+    ul.append(menor);
+
+    const parrafo2 = document.createElement("p");
+    parrafo2.textContent = "Números del menor al mayor: ";
+    document.body.append(parrafo2);
+
+    let ul2 = document.createElement("ul");
+    document.body.append(ul2);
+
+    const menor2 = document.createElement("li");
+    menor2.textContent = num3;
+    ul2.append(menor2);
+
+    const medio2 = document.createElement("li");
+    medio2.textContent = num2;
+    ul2.append(medio2);
+
+    const mayor2 = document.createElement("li");
+    mayor2.textContent = num1;
+    ul2.append(mayor2);
+
+    const parrafo3 = document.createElement("p");
+    parrafo3.textContent = sonIguales(num1, num2, num3);
+    document.body.append(parrafo3);
+}
